1d333232738de0d18c0bc136691156b4eabc9301
Eliana Angulo <eli15angulo@gmail.com>
2026-04-28 16:25:27 -0300

    herencia- abstract-virtual-protect

--- Poo-2026/Poo-2026/Ejemplo7.cs
+++ Poo-2026/Poo-2026/Ejemplo7.cs
@@ -7,40 +7,73 @@ namespace Poo_2026;
 //herencia
 public class Ejemplo7
 {
-    EntidadBase entidad = new EntidadBase();
+    //EntidadBase entidad = new EntidadBase();
     public Ejemplo7()
     {
         //instanciamos la clase base
-        entidad.Id = 10;
-        Console.WriteLine(entidad.MostrarId());
+       // entidad.Id = 10;
+      //  Console.WriteLine(entidad.MostrarId());
         //instanciamos la clase derivada
         Producto prod = new Producto();
         prod.NombreProducto = "Memoria Ram";
-        Console.WriteLine("---------------------");
-        Console.WriteLine(prod.MostrarId());
-        Console.WriteLine(prod.MostrarProducto());
+        //Console.WriteLine("---------------------");
+        //Console.WriteLine(prod.MostrarId());
+        //Console.WriteLine(prod.MostrarProducto());
         Console.WriteLine("---------------------");
         prod.Id = 20;
-        Console.WriteLine(entidad.MostrarId());
+        Console.WriteLine(prod.MostrarId());
         Console.WriteLine(prod.MostrarProducto());
+        prod.InclementarId();
     }
-
 }
-public class EntidadBase
+abstract public class EntidadBase
 {
     public int Id { get; set; }
     public string MostrarId()
     {
         return $"El id es: {Id}";
     }
-      
+    public virtual int InclementarId()
+    {
+        Id++;
+        return Id;
+    }
+    protected virtual int MetodoProtegido()
+    {
+        Console.WriteLine("Este es un método protegido");
+    }
+    abstract public int ReiniciarId();
 }
+public class Cliente : EntidadBase
+{
+    public string? NombreCliente { get; set; }
+    public string Mostrarcliente()
+    {
+        return $"El nombre del cliente es: {NombreCliente} con id: {Id}";
+    }
+    public override int ReiniciarId()
+    {
+        Id = 0;
+        return Id;
+    }
+    public override int InclementarId()
+    {
+        Id++;
+        return Id;
+    }
+
+} 
 public class Producto : EntidadBase
 {
     public string? NombreProducto { get; set; }
     public string MostrarProducto()
     {
         return $"El nombre del producto es: {NombreProducto} con id: {Id}";
+    }
+    public override int ReiniciarId()
+    {
+        Id = 0;
+        return Id;
     }
 }
 
